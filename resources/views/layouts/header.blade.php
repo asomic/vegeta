fab6d9bdac60667bfe459bc88843696c9d857260
boryeira <jmanuel.jorquera@gmail.com>
2020-07-12 22:07:12 -0400

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,12 +10,14 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
+                @auth
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('client.index')}}">Clientes</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('product.index')}}">Products</a>
                 </li>
+                @endAuth
             </ul>
 
             <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::resource('client','Client\ClientController');
 Route::resource('product','Product\ProductController');
